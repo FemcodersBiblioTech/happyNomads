469a13ce27200c0752d58c3479e6489d7d35a9b3
Sylvia Llorente <sylviall81@gmail.com>
2023-08-21 16:23:06 +0200

    practica tutorial Laravel chirper

---
+++ happyNomads/app/Http/Controllers/LocationController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\RedirectResponse;
+use App\Models\Location;
+use Illuminate\Http\Request;
+use Illuminate\Http\View;
+
+class LocationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): View
+    {
+        return view('locations.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        //
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
+ 
+        $request->user()->locations()->create($validated);
+ 
+        return redirect(route('locations.index'));
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Location $location)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Location $location)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Location $location)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Location $location)
+    {
+        //
+    }
+}
